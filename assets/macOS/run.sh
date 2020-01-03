4b5ba4590f9da67605086cc7f1ac880c82e1e61d
Levi506 <1168588-levi506@users.noreply.gitlab.com>
2020-01-03 09:10:50 +0000

    Update assets/macOS/run.sh

--- assets/macOS/run.sh
+++ assets/macOS/run.sh
@@ -3,6 +3,6 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$SCRIPTPATH"
 export WINEPREFIX="$SCRIPTPATH"
-"/Applications/Wine Staging.app/Contents/Resources/wine/bin/wine64" "$(pwd)/drive_c/Program Files (x86)/Pokemon Insurgence 1.2.5 Core/game.exe" &
+"/Applications/Wine Staging.app/Contents/Resources/wine/bin/wine64" "$(pwd)/drive_c/Program Files (x86)/Pokemon Insurgence 1.2.5 Core/Game.exe" &
 exit
 
