2a1243d61558b6593dc5620d6e8360e2d0dd446c
Levi506 <1168588-levi506@users.noreply.gitlab.com>
2020-03-27 01:29:41 +0000

    homebrew fix

--- assets/macOS/install.sh
+++ assets/macOS/install.sh
@@ -34,8 +34,7 @@ if [ -x "$(command -v brew)" ]; then
 else
     write_bold "Homebrew not installed, installing now"
     write_red_bold "Accept all prompts and provide your password when it's asked for"
-    HOMEBREW_INSTALL_SCRIPT=$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh) || exit 1
-    /usr/bin/ruby -e "$HOMEBREW_INSTALL_SCRIPT"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     brew analytics off
 fi
 
