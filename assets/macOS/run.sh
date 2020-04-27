76fe1028b4045a5c8d4fc58ca5d78391c1beafd7
Deukhoofd <deukhoofd@gmail.com>
2020-04-27 11:47:50 +0200

    Check whether wine and the game files exist on run

--- assets/macOS/run.sh
+++ assets/macOS/run.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
 
+# Exit on error
+set -e
+
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$SCRIPTPATH"
 export WINEPREFIX="$SCRIPTPATH"
-"/Applications/Wine Stable.app/Contents/Resources/wine/bin/wine" "$(pwd)/drive_c/Program Files (x86)/Pokemon Insurgence 1.2.5 Core/Game.exe" &
+
+# By default we'd prefer the use of the primary wine path.
+WINEPATH="wine"
+GAMEPATH="$(pwd)/drive_c/Program Files (x86)/Pokemon Insurgence 1.2.5 Core/Game.exe"
+# If the main wine path couldn't be found.
+if [ ! -x "$(command -v wine)" ]; then
+    # Attempt to resolve the full wine path
+    WINEPATH="/Applications/Wine Stable.app/Contents/Resources/wine/bin/wine"
+    # If this also doesn't exist, exit out.
+    if [ ! -f "$WINEPATH" ]; then
+        echo "Wine could not be found. This likely means your installation was incomplete."
+        exit 1
+    fi
+fi
+if [ ! -f "$GAMEPATH" ]; then
+    echo "The game files could not be found. This likely means your installation was incomplete."
+    exit 1
+fi
+
+
+$WINEPATH $GAMEPATH &
 exit
 
