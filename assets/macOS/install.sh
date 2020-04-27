2bdabf6ca675840d8fc97983b1ff96322ce9937a
Deukhoofd <deukhoofd@gmail.com>
2020-04-27 11:34:48 +0200

    Tell users their password is not visible while typing

--- assets/macOS/install.sh
+++ assets/macOS/install.sh
@@ -36,7 +36,7 @@ if [ -x "$(command -v brew)" ]; then
     write_bold "Homebrew is already installed! Good for you, Homebrew is awesome."
 else
     write_bold "Homebrew not installed, installing now"
-    write_red_bold "Accept all prompts and provide your password when it's asked for"
+    write_red_bold "Accept all prompts and provide your password when it's asked for. Note that no password will be shown while typing."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     brew analytics off
 fi
