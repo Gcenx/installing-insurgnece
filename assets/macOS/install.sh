7b56e72aa274510ee251b8f8bad34584d561d54b
Deukhoofd <deukhoofd@gmail.com>
2020-05-30 16:55:24 +0200

    Clearer final instructions on install

--- assets/macOS/install.sh
+++ assets/macOS/install.sh
@@ -93,10 +93,10 @@ rm -rf ~/.cache/wine ~/.cache/winetricks
 rm -rf $(brew --cache)
 
 print_step 10
-write_bold "Installing Insurg"
+write_bold "Installing Insurgence. This might take some time while it's downloading the game. Please be patient."
 curl -O "https://www.p-insurgence.com/releases/1.2.5/Pokemon Insurgence 1.2.5 Core.zip"
 unzip "Pokemon Insurgence 1.2.5 Core.zip" -d "$HOME/pkmn_insurg/drive_c/Program Files (x86)/"
 mv -f "Pokemon Insurgence 1.2.5 Core.zip" "$HOME/pkmn_insurg/drive_c/Program Files (x86)/"
 
 write_bold "Done!"
-write_red_bold "Wait for all Wine configuration to finish (wait for any remaining windows to close), then REBOOT and check the guide on the Pokemon Insurgence Forums for next steps"
+write_red_bold "Wait for all Wine configuration to finish (wait for any remaining windows to close), then REBOOT and run the game from the shortcut on your desktop!"
