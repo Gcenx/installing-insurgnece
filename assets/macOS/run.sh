9064c2696e4d0eb424583a28c7565d0c3be04405
Deukhoofd <deukhoofd@gmail.com>
2020-05-31 09:36:43 +0000

    Make run script not do weird things with pwd, just use directory we know the game is at

--- assets/macOS/run.sh
+++ assets/macOS/run.sh
@@ -3,13 +3,11 @@
 # Exit on error
 set -e
 
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd "$SCRIPTPATH"
-export WINEPREFIX="$SCRIPTPATH"
+export WINEPREFIX="$HOME/pkmn_insurg"
 
 # By default we'd prefer the use of the primary wine path.
 WINEPATH="wine"
-GAMEPATH="$(pwd)/drive_c/Program Files (x86)/Pokemon Insurgence 1.2.5 Core/Game.exe"
+GAMEPATH="$HOME/pkmn_insurg/drive_c/Program Files (x86)/Pokemon Insurgence 1.2.5 Core/Game.exe"
 # If the main wine path couldn't be found.
 if [ ! -x "$(command -v wine)" ]; then
     # Attempt to resolve the full wine path
