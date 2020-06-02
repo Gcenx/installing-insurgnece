eaf215cad907bb3da43db86379a95586fa656f08
Deukhoofd <deukhoofd@gmail.com>
2020-06-02 17:07:41 +0200

    Add script to reset run command

---
+++ assets/macOS/reset-run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
+curl -s -o "$HOME/pkmn_insurg/Run-Pokémon-Insurgence.command" "https://gitlab.com/levi506/installing-insurgnece/raw/master/assets/macOS/run.sh"
+chmod +x "$HOME/pkmn_insurg/Run-Pokémon-Insurgence.command"
